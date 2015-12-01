596bbf8d0a83cf2e358e46f0921c312e84441baf
Joseph <jhanson3@buffalo.edu>
2015-12-01 16:32:13 -0500

    Began development with a simple model to define selected notes on a fret board.

---
+++ src/tabcreator/TabCreator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tabcreator;
+
+/**
+ *
+ * @author jo27710
+ */
+public class TabCreator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interface IN = new Interface();
+        IN.setBounds(0, 0, 980, 670);
+        IN.setVisible(true);
+    }
+    
+}
